f3af2f1ecbd1d19607b2b97698e5a12518fbf40d
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2022-05-15 01:23:39 +0300

    updated dataset tests

--- tests/test_split_dir.py
+++ tests/test_split_dir.py
@@ -16,7 +16,20 @@ class TestSplitToDir(unittest.TestCase):
         dataset.split_to_directory()
         classes = dataset.get_classes()
 
-        self.assertIsNotNone(classes, "Slitting test failed")
+        self.assertIsNotNone(classes, f"Splitting test failed: {classes}")
+
+        dataset2 = Dataset(
+            dataset_path=r'..\\dev\\dataset', 
+            csv_path=r'..\\dev\\test.csv',
+            output_directory=None,
+            output_format="df")
+
+        df, x_col, y_col = dataset2.split_to_df()
+
+        self.assertIsNotNone(df, "DF Splitting test failed")
+        self.assertIsNotNone(x_col, f"Splitting test {x_col} failed")
+        self.assertIsNotNone(y_col, f"Splitting test {y_col} failed")
+
 
 if __name__ == "__main__":
     unittest.main()
